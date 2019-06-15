0b2df301509290d148793e663a0d7face7bb9d46
ArmenHagopian <ArmenHagopian@users.noreply.github.com>
2019-06-15 21:19:01 +0200

    First commit

---
+++ drive.py
@@ -0,0 +1,71 @@
+#!/usr/local/bin/python3
+""" Connects to the simulator and runs the model """
+import io
+import base64
+import socketio
+import eventlet
+import numpy as np
+from PIL import Image
+from simple_pid import PID
+from keras.models import load_model
+from keras.applications.vgg16 import preprocess_input
+from keras.preprocessing.image import img_to_array
+
+# Handle to connect to the simulator
+SIO = socketio.Server()
+# ML handle to predict the steering
+MODEL = None
+# Speed control with PID controller
+REGUL = PID(1, 0.7, 0, setpoint=15, output_limits=(0, 1))
+
+
+@SIO.on('telemetry')
+def telemetry(_, data):
+    """ Event sent by the simulator. """
+    def preprocess(image):
+        """
+        First, convert the image from a PIL object to a numpy array.
+        Crop the sky from the image by removing the top 60px and then use the
+        VGG16 preprocess method to distribute the inputs between [-1.0;1.0].
+        """
+        img = img_to_array(image)
+        return preprocess_input(img, mode='tf', data_format='channels_last')
+
+    if data:
+        # The current speed of the car
+        speed = REGUL(float(data["speed"]))
+        # The current image from the center camera of the car
+        image = Image.open(io.BytesIO(base64.b64decode(data["image"])))
+        # Use your model to compute steering and throttle
+        feats = np.expand_dims(preprocess(image), axis=0)
+        steer = MODEL.predict(feats).flatten()[0]
+        print("steer: {:f} throttle: {:f}".format(steer, speed))
+        # Response to the simulator with a steer angle and throttle
+        send(steer, speed)
+    else:
+        # Edge case
+        SIO.emit('manual', data={}, skip_sid=True)
+
+
+@SIO.on('connect')
+def connect(sid, _):
+    """ Event fired when simulator connect. """
+    print("connect ", sid)
+    send(0, 0)
+
+
+def send(steer, throttle):
+    """ To send steer angle and throttle to the simulator. """
+    SIO.emit("steer", data={'steering_angle': str(steer),
+                            'throttle': str(throttle)},
+             skip_sid=True)
+
+
+if __name__ == '__main__':
+    MODEL = load_model('model.h5')
+    MODEL.summary()
+
+    # wrap with a WSGI application
+    APP = socketio.WSGIApp(SIO)
+    # simulator will connect to localhost:4567
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), APP)
---
+++ image.py
@@ -0,0 +1,191 @@
+#!/usr/local/bin/python3
+"""
+image.py -- Convolutional Neural Network trainer for self-driving cars.
+Copyright (C) 2019  Alexis Nootens & Armen Hagopian
+
+Licensed under the EUPL 1.2
+Read license.txt for more details.
+"""
+import argparse
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from keras.applications.vgg16 import preprocess_input
+from keras.callbacks import CSVLogger, EarlyStopping
+from keras.layers import Conv2D, Cropping2D, Dense, Dropout, Flatten
+from keras.models import Sequential, load_model
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import Sequence
+
+
+class TrackGenerator(Sequence):
+    """
+    This class allows to combine the center, left, and right image inside
+    the same training batch. It also allows to create several workers to
+    multithread the training process.
+    """
+
+    def __init__(self, dataframe, preprocess, batch_size):
+        self.dataframe = dataframe
+        self.preprocess = preprocess
+        self.batch_size = batch_size
+
+        self.datagen = ImageDataGenerator(
+            fill_mode='reflect',
+            width_shift_range=0.1,
+            height_shift_range=0.1,
+            data_format='channels_last',
+            preprocessing_function=preprocess,
+            dtype=np.float32
+        )
+
+        self.camera_cent = self.datagen.flow_from_dataframe(
+            dataframe=dataframe, x_col='Center', y_col='Steering',
+            target_size=(160, 320), class_mode='other', shuffle=True,
+            batch_size=batch_size, interpolation='bilinear')
+        self.camera_righ = self.datagen.flow_from_dataframe(
+            dataframe=dataframe, x_col='Right', y_col='Steering',
+            target_size=(160, 320), class_mode='other', shuffle=True,
+            batch_size=batch_size, interpolation='bilinear')
+        self.camera_left = self.datagen.flow_from_dataframe(
+            dataframe=dataframe, x_col='Left', y_col='Steering',
+            target_size=(160, 320), class_mode='other', shuffle=True,
+            batch_size=batch_size, interpolation='bilinear')
+
+    def __getitem__(self, idx):
+        sample_cent = self.camera_cent.__getitem__(idx)
+        sample_left = self.camera_left.__getitem__(idx)
+        sample_righ = self.camera_righ.__getitem__(idx)
+        camera_comb = np.vstack((sample_cent[0], sample_left[0]))
+        camera_comb = np.vstack((camera_comb, sample_righ[0]))
+        rotate_comb = np.hstack((sample_cent[1], sample_left[1]+.25))
+        rotate_comb = np.hstack((rotate_comb, sample_righ[1]-0.25))
+        return camera_comb, rotate_comb
+
+    def __len__(self):
+        return self.dataframe.shape[0] // self.batch_size
+
+
+def create_model():
+    """ Returns a compiled model """
+    model = Sequential([
+        Cropping2D(cropping=((60, 0), (0, 0)), input_shape=(160, 320, 3),
+                   data_format='channels_last'),
+        Conv2D(24, kernel_size=5, strides=2, activation='relu'),
+        Dropout(0.2),
+        Conv2D(36, kernel_size=5, strides=2, activation='relu'),
+        Dropout(0.2),
+        Conv2D(48, kernel_size=5, strides=2, activation='relu'),
+        Dropout(0.2),
+        Conv2D(64, kernel_size=3, strides=1, activation='relu'),
+        Dropout(0.2),
+        Conv2D(64, kernel_size=3, strides=1, activation='relu'),
+        Dropout(0.2),
+        Flatten(),
+        Dense(100),
+        Dense(50),
+        Dense(10),
+        Dense(1)
+    ])
+    optimizer = RMSprop(lr=1e-6)
+    model.compile(optimizer, loss='mse', metrics=['mae', 'mse'])
+    return model
+
+
+def train_model(model, dataframe):
+    """
+    Fit the model with the dataframe. If no model is provided then load 'model.h5'.
+    The model is trained with batches of the inputs whilst augmenting the data
+    (i.e. rotating the image from 0 to 5°). The RGB pixels from 0 to 255 are
+    linearly translated to the range [-1.0;1.0].
+    """
+    def preprocess(image):
+        """ Distribute the inputs between [-1.0;1.0] """
+        return preprocess_input(image, mode='tf', data_format='channels_last')
+
+    early_stop = EarlyStopping(monitor='mean_squared_error', patience=20,
+                               mode='min', restore_best_weights=True)
+    csv_logger = CSVLogger('model_history_log.csv')
+    images_gen = TrackGenerator(dataframe, preprocess, 32)
+    VALIDATION_LOG = read_driving_log('validation_log.csv')
+    images_validation = TrackGenerator(VALIDATION_LOG, preprocess, 32)
+    model.fit_generator(images_gen, epochs=15, validation_data=images_validation,
+                        validation_steps=32,
+                        workers=20, max_queue_size=500,
+                        callbacks=[early_stop, csv_logger])
+
+    model.save('model.h5')
+    print("[INFO] model saved")
+
+
+def read_driving_log(filename):
+    """ Returns a dataframe with only the images paths and the steering values """
+    column_names = ['Center', 'Left', 'Right', 'Steering',
+                    'Accelerator', 'Brake', 'Speed']
+    dataset = pd.read_csv(filename, names=column_names)
+    dataset = dataset.drop(columns=['Accelerator', 'Brake', 'Speed'])
+    dataset['Steering'] = dataset['Steering'].astype(np.float32)
+    # Keep only one out of five frames
+    to_take = range(0, dataset.shape[0], 5)
+    dataset = dataset.take(to_take)
+    return dataset
+
+
+def plot_history(hist):
+    """ Plot the MAE and MSE progress during training with matplotlib. """
+
+    """ MAE """
+
+    plt.subplot(2, 2, 1)
+    plt.xlabel('Batch')
+    plt.ylabel('Mean Abs Error [Steering angle]')
+    plt.plot(hist['epoch'], hist['mean_absolute_error'], label='Train Error')
+    # plt.plot(hist['epoch'], hist['val_mean_absolute_error'], label='Val Error')
+    plt.legend()
+
+    """ MSE """
+
+    plt.subplots_adjust(wspace=0.3)
+    plt.subplot(2, 2, 2)
+    plt.xlabel('Batch')
+    plt.ylabel('Mean Square Error [Steering angle$^2$]')
+    plt.plot(hist['epoch'], hist['mean_squared_error'], label='Train Error')
+    # plt.plot(hist['epoch'], hist['val_mean_squared_error'], label='Val Error')
+    plt.legend()
+
+    """ Plot the validation MAE and MSE progress with matplotlib. """
+
+    """ Validation MAE """
+
+    plt.subplots_adjust(wspace=0.3)
+    plt.subplot(2, 2, 3)
+    plt.xlabel('Batch')
+    plt.ylabel('Validation Mean Abs Error [Steering angle$^2$]')
+    plt.plot(hist['epoch'], hist['val_mean_absolute_error'], label='Validation Error')
+    plt.legend()
+
+    """ Validation MSE """
+
+    plt.subplots_adjust(wspace=0.3)
+    plt.subplot(2, 2, 4)
+    plt.xlabel('Batch')
+    plt.ylabel('Validation Mean Square Error [Steering angle$^2$]')
+    plt.plot(hist['epoch'], hist['val_mean_squared_error'], label='Validation Error')
+    plt.legend()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(description='Train a self-driving car.')
+    PARSER.add_argument('-c', action='store_true', help='Create a new model')
+    ARGS = PARSER.parse_args()
+
+    MODEL = create_model() if ARGS.c else load_model('model.h5')
+    DRV_LOG = read_driving_log('driving_log.csv')
+    train_model(MODEL, DRV_LOG)
+
+    HISTORY = pd.read_csv('model_history_log.csv')
+    plot_history(HISTORY)
